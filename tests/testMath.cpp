c600a3a4af3f28133e4331411459f66115d41712
Akarsh <akarshmishra.tech@gmail.com>
2026-05-14 03:32:13 +0530

    Added google tests.

---
+++ tests/testMath.cpp
@@ -0,0 +1,11 @@
+
+#include <gtest/gtest.h>
+#include "mathUtils.h"
+
+TEST(MathTest, AdditionTest) {
+    EXPECT_EQ(add(2, 3), 5);
+}
+
+TEST(MathTest, SubtractionTest) {
+    EXPECT_EQ(subtract(10, 4), 6);
+}
